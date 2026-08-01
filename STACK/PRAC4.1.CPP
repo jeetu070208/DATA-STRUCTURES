2fed82a948ba1ad61546c97739ee55a0c2113f6b
jeetu070208 <jeetugo4m@gmail.com>
2026-08-01 13:44:44 +0530

    Implement infix to postfix conversion in C++

---
+++ STACK/PRAC4.1.CPP
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+
+int precedence(char op)
+{
+    if(op == '^')
+        return 3;
+    else if(op == '*' || op == '/')
+        return 2;
+    else if(op == '+' || op == '-')
+        return 1;
+    else
+        return 0;
+}
+
+
+string infixToPostfix(string infix)
+{
+    stack<char> s;
+    string postfix = "";
+
+    for(int i = 0; i < infix.length(); i++)
+    {
+        char ch = infix[i];
+
+        
+        if(isalnum(ch))
+        {
+            postfix += ch;
+        }
+
+        
+        else if(ch == '(')
+        {
+            s.push(ch);
+        }
+
+        
+        else if(ch == ')')
+        {
+            while(!s.empty() && s.top() != '(')
+            {
+                postfix += s.top();
+                s.pop();
+            }
+            s.pop();
+        }
+
+        
+        else
+        {
+            while(!s.empty() && precedence(s.top()) >= precedence(ch))
+            {
+                postfix += s.top();
+                s.pop();
+            }
+            s.push(ch);
+        }
+    }
+
+    while(!s.empty())
+    {
+        postfix += s.top();
+        s.pop();
+    }
+
+    return postfix;
+}
+
+int main()
+{
+    string infix;
+
+    cout << "Enter Infix Expression: ";
+    cin >> infix;
+
+    cout << "Postfix Expression: " << infixToPostfix(infix);
+
+    return 0;
+}
